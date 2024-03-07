ac93cc05739895677500205d7a581600e8625959
sri-yeah <sriyesh62@gmail.com>
2024-03-07 20:46:03 +0000

    removed circular dependancy
    everytime i make a change i swear i jsut increase the number of errors how does this work

--- include/ast_context.hpp
+++ include/ast_context.hpp
@@ -3,7 +3,6 @@
 
 #include <unordered_map>
 #include <string>
-#include "ast_node.hpp"
 
 // An object of class Context is passed between AST nodes during compilation.
 // This can be used to pass around information about what's currently being
