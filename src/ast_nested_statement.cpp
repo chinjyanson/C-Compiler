456928322becab69d5ed9595c17933f3f00e2dd9
chinjyanson <anson.chin22@imperial.ac.uk>
2024-03-22 19:23:46 +0000

    [ayc122] dealing with merge conflicts

--- src/ast_nested_statement.cpp
+++ src/ast_nested_statement.cpp
@@ -2,7 +2,6 @@
 
 void NestStatement::EmitRISC(std::ostream &stream, Context &context, int destReg) const {
     bool was_function = context.is_function;
-    context.is_function = false;
     branch_->EmitRISC(stream, context, destReg);
     context.is_function = was_function;
 }
